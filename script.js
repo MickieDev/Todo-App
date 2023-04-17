b63154f22f600f302b1cc894aedba33ec6c7fea0
MickieDev <knissayan@gmail.com>
2023-04-17 21:10:27 +0700

    add code

---
+++ script.js
@@ -0,0 +1,51 @@
+
+let enterButton = document.getElementById("enter")
+let input = document.getElementById("userInput")
+let ul = document.querySelector("ul")
+let item = document.getElementsByTagName("li")
+
+
+function inputLength() {
+    return input.value.length
+}
+
+function listLength() {
+    return item.length
+}
+
+function createListElement() {
+    var li = document.createElement("li")
+    li.appendChild(document.createTextNode(input.value))
+    ul.appendChild(li)
+    input.value = ""
+
+    function isDone() {
+        li.classList.toggle("done")
+    }
+    li.addEventListener('click', isDone)
+
+    var deleteBtn = document.createElement("button")
+    deleteBtn.appendChild(document.createTextNode("X"))
+    li.appendChild(deleteBtn)
+    deleteBtn.addEventListener("click", deleteListItem)
+
+    function deleteListItem() {
+        li.classList.add("delete")
+    }
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement()
+    }
+}
+
+function addListAfterKeypress(event) {
+    if (inputLength() > 0 && event.which === 13) {
+        createListElement()
+    }
+}
+
+enterButton.addEventListener("click", addListAfterClick)
+input.addEventListener("keypress", addListAfterKeypress)
+
